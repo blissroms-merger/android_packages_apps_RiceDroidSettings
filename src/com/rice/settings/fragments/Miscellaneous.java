4a68928f6c5af793286fcecb19bdb0843c8ef629
pimpmaneaton <shaunsibu@gmail.com>
2022-07-31 14:31:05 -0600

    Goodbye photos hack

--- src/com/rice/settings/fragments/Miscellaneous.java
+++ src/com/rice/settings/fragments/Miscellaneous.java
@@ -47,14 +47,11 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     private static final String POCKET_JUDGE = "pocket_judge";
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
-    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
 
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
 
     private Preference mPocketJudge;
     private SwitchPreference mGamesSpoof;
-    private SwitchPreference mPhotosSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,9 +72,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
         mGamesSpoof.setOnPreferenceChangeListener(this);
 
-        mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
-        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
-        mPhotosSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -85,10 +79,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         if (preference == mGamesSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
-            return true;
-        } else if (preference == mPhotosSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
         }
         return false;
